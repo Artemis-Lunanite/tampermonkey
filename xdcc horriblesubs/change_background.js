a730fd79c3ed6987c1c2be6e2f216224b1568853
Kyle Crossman <Kcross119@yahoo.ca>
2016-11-06 19:57:26 -0500

    Added new script.

---
+++ xdcc horriblesubs/change_background.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         HorribleSubs Change Background
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Changes the background on xdcc.horriblesubs.info.
+// @author       CrunchyHotDogs
+// @match        http://xdcc.horriblesubs.info/
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/xdcc_horriblesubs/change_background.js
+// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/xdcc_horriblesubs/change_background.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+	javascript:p.setSkin(3);
+})();
