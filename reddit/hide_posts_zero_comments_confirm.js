8526c034ae33258a0c67131740a0f126f5260ff9
Kyle Crossman <Kcross119@yahoo.ca>
2016-12-07 12:14:51 -0500

    Ask User
    Asks the user before hiding all posts with zero comments.

---
+++ reddit/hide_posts_zero_comments_confirm.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Reddit Hide Posts No Comments - Confirm
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Hides posts with 0 comments. Asks the user everytime the page is loaded.
+// @author       CrunchyHotDogs
+// @match        https://www.reddit.com/r/*
+// @exclude      https://www.reddit.com/r/*/comments/*
+// @exclude      https://www.reddit.com/r/*/submit
+// @grant        none
+// @require 	 http://code.jquery.com/jquery-latest.js
+// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/reddit/hide_posts_zero_comments_confirm.js
+// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/reddit/hide_posts_zero_comments_confirm.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function hideComments() {
+        $('.thing').each(function(i, div) {
+            var comments = $(this).find('.comments');
+            var thing = $(this);
+            if(comments.text() === 'comment') {
+                thing.hide();
+            }
+        });
+    }
+	var r = confirm('Hide posts with zero comments');
+    if (r) {
+		hideComments();
+	{
+})();
