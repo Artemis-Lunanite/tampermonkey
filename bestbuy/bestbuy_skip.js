7793a86b59ea3373fd35f2f6bb186ba092faedf4
Kyle Crossman <Kcross119@yahoo.ca>
2017-04-26 12:07:26 -0400

    Added a new script.

---
+++ bestbuy/bestbuy_skip.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         BestBuy Skip Canada Cares
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Once you enter your BestBuy codes on the receipt skip the rest of the survey.
+// @author       CrunchyHotDogs
+// @match        https://survey.confirmit.com/wix/0/*
+// @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
+// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/bestbuy/bestbuy_skip.js
+// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/bestbuy/bestbuy_skip.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+})();
