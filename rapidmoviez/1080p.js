d853ce8f866d9f25df6617a5f08953c529015d4a
Kyle Crossman <Kcross119@yahoo.ca>
2017-05-26 19:58:32 -0400

    Update 1080p.js

--- rapidmoviez/1080p.js
+++ rapidmoviez/1080p.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RMZ 1080p
 // @namespace    http://tampermonkey.net/
-// @version      1.36
+// @version      1.37
 // @description  Hides all releases from rmz.cr that are not 1080p.
 // @author       CrunchyHotDogs
 // @match        *://rmz.cr/*
@@ -17,9 +17,9 @@
 		ep.each(function(i, div) {
             var li = $(this);
 			var item = li.find('a').text();
-            console.log(search(item,'1080p'))
+            console.log(i + search(item,'1080p') + '_' + search(item,'HEVC'))
 			if (search(item,'1080p') && search(item,'HEVC')) {
-                li.hide();
+                //li.hide();
             }
         });
     }
