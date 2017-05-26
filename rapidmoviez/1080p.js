a72f717a40a823b53535d5b0b60eb34a1abfcf1d
Kyle Crossman <Kcross119@yahoo.ca>
2017-05-26 11:53:16 -0400

    New Script For RMZ

---
+++ rapidmoviez/1080p.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         RMZ 1080p
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Hides all releases from rmz.cr that are not 1080p.
+// @author       CrunchyHotDogs
+// @match        https://rmz.cr/*
+// @grant        none
+// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/rapidmoviez/1080p.js
+// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/rapidmoviez/1080p.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function save1080p() {
+        var ep = $('#episodes li');
+		ep.each(function(i, div) {
+            var li = $(this);
+			var item = li.find('a');
+            
+            if(item.indexOf('1080p') >= 0) {
+                li.hide();
+            }
+        });
+    }
+    save1080p();
+})();
