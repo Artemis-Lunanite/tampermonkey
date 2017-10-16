5f52394053ecd301357d11a578a4606430f6c818
Kyle Crossman <Kcross119@yahoo.ca>
2017-10-16 12:41:56 -0400

    Changed Scroll Distance

--- goodreads/scroll_fix.js
+++ goodreads/scroll_fix.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Goodreads Scroll Fix
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  When you edit a record in your Goodreads shelf, usually it scrolls back to the top of the page. This makes it so the page stays in the same place.
 // @author       CrunchyHotDogs
 // @match        https://www.goodreads.com/review/list/*
@@ -21,7 +21,7 @@
                 jQuery(this).click(function() {
                     console.log(recordId);
                     jQuery('html, body').animate({
-                        scrollTop: jQuery(recordId).offset().top - 100
+                        scrollTop: jQuery(recordId).offset().top - 175
                     }, 10);
                 });
             });
