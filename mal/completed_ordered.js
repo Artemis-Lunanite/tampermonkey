490570db674e1b43b462767c6ea156f495bd0fa9
Kyle Crossman <Kcross119@yahoo.ca>
2017-12-01 21:14:07 -0500

    Add files via upload

---
+++ mal/completed_ordered.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         MAL Add Completed Order Button
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  CURRENTLY ONLY WORKS WITH MY PROFILE. Creates a button that will show the completed list, but sorted by finished date descending.
+// @author       CrunchyHotDogs
+// @match        *https://myanimelist.net/animelist/*
+// @grant        none
+// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/mal/completed_ordered.js
+// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/mal/completed_ordered.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function createButton() {
+		console.log(window.location.href);
+		var inserted = false;
+        var statusMenu = $('#status-menu').children();
+		statusMenu = statusMenu.children();
+		statusMenu.each(function(i, ele) {
+			if ($(this).is('div') && inserted === false) {
+				$('<a href="https://myanimelist.net/animelist/CrunchyHotDogs?order=2&status=2" class="status-button">Completed Ordered</a>').insertBefore(ele);
+				inserted = true;
+			}
+        });
+    }
+
+    createButton();
+})();
