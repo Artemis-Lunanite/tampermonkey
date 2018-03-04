4cb613652422cdeba5b6773e30a00873e9669702
Kyle Crossman <Kcross119@yahoo.ca>
2018-03-04 14:13:04 -0500

    Add files via upload

---
+++ thetvdb/stop_banner_rotate.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         TheTVDB Banner Rotate Stop
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Stops the stupid banner from rotating, thus preventing the webpage from jumping around.
+// @author       CrunchyHotDogs
+// @match        https://www.thetvdb.com*
+// @grant        none
+// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/thetvdb/stop_banner_rotate.js
+// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/thetvdb/stop_banner_rotate.js
+// ==/UserScript==
+
+(function() {
+	'use strict';
+	function so_xfade() { }
+})();
+
