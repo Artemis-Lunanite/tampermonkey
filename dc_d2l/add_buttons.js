29ffdc9f63556eb9e3d12c3e7260391992ecda88
Kyle Crossman <Kcross119@yahoo.ca>
2018-04-12 12:51:42 -0400

    Update add_buttons.js
    Added new link for Library of Congress Authorities.

--- dc_d2l/add_buttons.js
+++ dc_d2l/add_buttons.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DC Desire2Learn Add Email
 // @namespace    http://tampermonkey.net/
-// @version      1.3.0
+// @version      1.3.1
 // @description  Adds a button to DC D2L that takes the user to their DC mail.
 // @author       CrunchyHotDogs
 // @match        https://durhamcollege.desire2learn.com/*
@@ -11,9 +11,26 @@
 // ==/UserScript==
 
 (function() {
+	//Links
 	var email = 'https://outlook.office.com/owa/?realm=dcmail.ca&vd=www';
     	var library = 'http://guides.library.durhamcollege.ca/?b=s';
-	var html = '<div style="display: inline-block; vertical-align: middle; padding-right: 20px;"><a href="' + library + '" target="_blank" style="color: #CEE6DF; font-size: 1.05em; font-weight: bold; padding-right: 10px;">Go To Library</a><a href="' + email + '" target="_blank" style="color: #CEE6DF; font-size: 1.05em; font-weight: bold;">Go To Email</a></div>';
+	var loc = 'http://authorities.loc.gov';
+	
+	//CSS
+	var css_link = 'color: #CEE6DF; font-size: 1.05em; font-weight: bold; padding-right: 10px;';
+	
+	//HTML
+	var html = 	'<div style="display: inline-block; vertical-align: middle; padding-right: 20px;">' +
+				'<a href="' + library + '" target="_blank" style="' + css_link + '">' +
+	    				'Library' +
+	    			'</a>' +
+				'<a href="' + loc + '" target="_blank" style="' + css_link + '">' +
+	    				'LOC Authorities' +
+				'</a>' +
+	    			'<a href="' + email + '" target="_blank" style="' + css_link + '">' +
+	    				'Email' +
+				'</a>' +
+	    		'</div>';
 
 
     
