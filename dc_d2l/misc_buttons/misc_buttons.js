0ea5a00c40a5f4cf020bcda6dfaf2b55b3f7f628
Kyle Crossman <Kcross119@yahoo.ca>
2018-09-08 17:36:27 -0400

    Completed Script

--- dc_d2l/misc_buttons/misc_buttons.js
+++ dc_d2l/misc_buttons/misc_buttons.js
@@ -1,42 +1,55 @@
 // ==UserScript==
-// @name         DC Desire2Learn Add Email
+// @name         DC Desire2Learn Add Misc Buttons
 // @namespace    http://tampermonkey.net/
-// @version      1.3.1
-// @description  Adds a button to DC D2L that takes the user to their DC mail.
+// @version      1.1
+// @description  Adds menu in Desire2Learn that contains useful links.
 // @author       CrunchyHotDogs
 // @match        https://durhamcollege.desire2learn.com/*
-// @grant        none
-// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/dc_d2l/add_buttons.js
-// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/dc_d2l/add_buttons.js
+// @grant        GM_addStyle
+// @grant		 GM_getResourceText
+// @resource	 html https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/dc_d2l/misc_buttons/template.html
+// @downloadUrl  https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/dc_d2l/misc_buttons/misc_buttons.js
+// @updateUrl	 https://raw.githubusercontent.com/CrunchyHotDogs/tampermonkey/master/dc_d2l/misc_buttons/misc_buttons.js
 // ==/UserScript==
 
+
+GM_addStyle('body{background:#a0a0a0;padding:45px 45px 45px 300px;height:500px}.kc_header_container{display:inline-block;position:relative;height:100%;margin:0 15px}.kc_header_button{align-items:center;border:none;color:#565a5c;cursor:pointer;display:flex;font-family:inherit;font-size:.7rem;height:90px;margin:0;overflow:visible;padding:0;position:relative;background-color:#fff}.kc_header_button .kc_hover_indicator,.kc_hover{border-bottom-left-radius:4px;border-bottom-right-radius:4px}.kc_header_button:hover .kc_hover_indicator{display:block}.kc_header_button:hover .kc_no_hover_image{display:none}.kc_header_button:hover .kc_hover_image{display:inline-block}.kc_header_button .kc_hover_image{display:none}.kc_header_button .kc_hover_indicator{position:absolute;display:none;top:0;height:4px;width:30px;background-color:#005694}.kc_hover,.kc_triangle{top:-15px;position:absolute}.kc_header_button img{width:30px;height:30px}.kc_hover{width:40px;height:10px;background:#005694}.kc_triangle{width:0;height:0;left:75px;background-color:transparent;border-bottom:15px solid #e6e6e6;border-left:15px solid transparent;border-right:15px solid transparent}.kc_popup_container{position:absolute;left:-75px;width:190px;padding-top:5px;background-color:#f3f3f3;box-sizing:border-box;color:#565a5c;z-index:1000;border:3px solid #e6e6e6;border-radius:5px;text-align:center}.kc_popup_container .kc_menu_button{display:inline-block;margin:5px;padding:5px;border-radius:5px;width:30px;height:30px;}.kc_popup_container .kc_menu_button:hover{background-color:#d8e5f2}.hide{display:none!important}');
+
 (function() {
-	//Links
-	var email = 'https://outlook.office.com/owa/?realm=dcmail.ca&vd=www';
-    	var library = 'http://guides.library.durhamcollege.ca/?b=s';
-	var loc = 'http://authorities.loc.gov';
-	
-	//CSS
-	var css_link = 'color: #CEE6DF; font-size: 1.05em; font-weight: bold; padding-right: 10px;';
-	
-	//HTML
-	var html = 	'<div style="display: inline-block; vertical-align: middle; padding-right: 20px;">' +
-				'<a href="' + library + '" target="_blank" style="' + css_link + '">' +
-	    				'Library' +
-	    			'</a>' +
-				'<a href="' + loc + '" target="_blank" style="' + css_link + '">' +
-	    				'LOC Authorities' +
-				'</a>' +
-	    			'<a href="' + email + '" target="_blank" style="' + css_link + '">' +
-	    				'Email' +
-				'</a>' +
-	    		'</div>';
-
-
-    
-    function addButton() {
-        var mainDiv = $('.d2l-minibar-right');
-        mainDiv.prepend(html);
-    }
-    addButton();
+    function addButtons(callback) {
+		var mainDiv = $('.d2l-navigation-s-header-actions');
+		var lastDiv = mainDiv.children().last();
+		lastDiv.before(GM_getResourceText(('html')));
+		callback();
+	}
+	//Callback function to add click listeners.
+	function addOnClickListener() {
+		//Opens the popup.
+		$('#kc_button').click(function() {
+			if ($('#kc_popup').hasClass('hide')) {
+				$('#kc_popup').removeClass('hide');
+			}
+			else {
+				$('#kc_popup').addClass('hide');
+			}
+			return false;
+		});	
+		
+		//Closes the popup.
+		$('body').click(function(evt){    
+			if(evt.target.id == "kc_popup") {
+				return;
+			}
+			//For descendants of menu_content being clicked, remove this check if you do not want to put constraint on descendants.
+			if($(evt.target).closest('#kc_popup').length) {
+				return;             
+			}
+			//Do processing of click event here for every element except with id menu_content
+			if ($('#kc_popup').hasClass('hide')) {}
+			else {
+				$('#kc_popup').addClass('hide');
+			}
+		});
+	}
+    addButtons(addOnClickListener);
 })();
