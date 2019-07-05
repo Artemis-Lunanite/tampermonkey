2dd37960f115937e6c418d1b66dac6bc981f647a
Kyle Crossman <kcross199@gmail.com>
2019-07-05 13:34:11 -0400

    Update completed_ordered.js

--- mal/completed_ordered.js
+++ mal/completed_ordered.js
@@ -3,12 +3,12 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.15
 // @description  CURRENTLY ONLY WORKS WITH MY PROFILE. Creates a button that will show the completed list, but sorted by finished date descending.
-// @author       Artemis Lunarly
+// @author       Artemis Lunanite
 // @icon         http://i.imgur.com/b7Fw8oH.png
 // @match        *https://myanimelist.net/animelist/*
 // @grant        none
-// @downloadUrl  https://raw.githubusercontent.com/Artemis-Lunarly/tampermonkey/master/mal/completed_ordered.js
-// @updateUrl	 https://raw.githubusercontent.com/Artemis-Lunarly/tampermonkey/master/mal/completed_ordered.js
+// @downloadUrl  https://raw.githubusercontent.com/Artemis-Lunanite/tampermonkey/master/mal/completed_ordered.js
+// @updateUrl	 https://raw.githubusercontent.com/Artemis-Lunanite/tampermonkey/master/mal/completed_ordered.js
 // ==/UserScript==
 
 (function() {
@@ -19,7 +19,7 @@
 		statusMenu = statusMenu.children();
 		statusMenu.each(function(i, ele) {
 			if ($(this).is('div') && inserted === false) {
-				$('<a href="https://myanimelist.net/animelist/Artemis-Lunarly?order=2&status=2" class="status-button">Completed Ordered</a>').insertBefore(ele);
+				$('<a href="https://myanimelist.net/animelist/Artemis-Lunanite?order=2&status=2" class="status-button">Completed Ordered</a>').insertBefore(ele);
 				inserted = true;
 			}
         });
