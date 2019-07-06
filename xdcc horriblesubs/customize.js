edf888ff0a0dea97b43b674e9d97e024447d4bf8
Kyle Crossman <kcross199@gmail.com>
2019-07-06 09:30:27 -0400

    Update customize.js

--- xdcc horriblesubs/customize.js
+++ xdcc horriblesubs/customize.js
@@ -3,12 +3,12 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.56118
 // @description  Changes the background on xdcc.horriblesubs.info.
-// @author       Artemis Lunarly
+// @author       Artemis Lunanite
 // @match        *xdcc.horriblesubs.info/
 // @grant        GM_addStyle
 // @require      http://code.jquery.com/jquery-latest.js
-// @downloadUrl  https://raw.githubusercontent.com/Artemis-Lunarly/tampermonkey/master/xdcc%20horriblesubs/customize.js
-// @updateUrl	 https://raw.githubusercontent.com/Artemis-Lunarly/tampermonkey/master/xdcc%20horriblesubs/customize.js
+// @downloadUrl  https://raw.githubusercontent.com/Artemis-Lunanite/tampermonkey/master/xdcc%20horriblesubs/customize.js
+// @updateUrl	 https://raw.githubusercontent.com/Artemis-Lunanite/tampermonkey/master/xdcc%20horriblesubs/customize.js
 // ==/UserScript==
 
 GM_addStyle('.header h1 {display: inline !important; } .kc_container{ margin-left: 150px; display: inline; verticle-align: middle; } .kc_button{ margin-left: 15px; display: inline; height: 50%; border-radius: 50px; background-color: black; color: white; font-size: 1.25em; }');
