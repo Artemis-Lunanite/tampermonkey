13ef4d5c892c545d499433b254c946bd61eb1097
Kyle Crossman <kcross199@gmail.com>
2019-09-22 22:10:03 -0400

    Update customize.js

--- xdcc horriblesubs/customize.js
+++ xdcc horriblesubs/customize.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HorribleSubs Customize
 // @namespace    http://tampermonkey.net/
-// @version      1.56118
+// @version      1.56119
 // @description  Changes the background on xdcc.horriblesubs.info.
 // @author       Artemis Lunanite
 // @match        *xdcc.horriblesubs.info/
@@ -19,7 +19,10 @@ GM_addStyle('.header h1 {display: inline !important; } .kc_container{ margin-lef
         ['Dr. Stone', 'Dr. Stone 720'],
 	['Fire Force', 'Enen no Shouboutai 720'],
 	['BEM', 'BEM 720'],
-	['Vinland', 'Vinland Saga 720']];
+	['Vinland', 'Vinland Saga 720'],
+	['SAO', 'Sword Art Online War of Underworld 720'],
+	['Food Wars', 'Shokugeki no Souma: Shin no Sara 720'],
+	['Fairy Gone', 'Fairy Gone S2 720']];
 
 
     function addAnimeButtons(callback) {
