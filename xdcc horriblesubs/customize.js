07a7a799ad4702f6f3c531b5320aa2308419a300
Kyle Crossman <kcross199@gmail.com>
2019-11-10 15:54:02 -0500

    Update customize.js

--- xdcc horriblesubs/customize.js
+++ xdcc horriblesubs/customize.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HorribleSubs Customize
 // @namespace    http://tampermonkey.net/
-// @version      1.56119
+// @version      1.56120
 // @description  Changes the background on xdcc.horriblesubs.info.
 // @author       Artemis Lunanite
 // @match        *xdcc.horriblesubs.info/
@@ -21,7 +21,7 @@ GM_addStyle('.header h1 {display: inline !important; } .kc_container{ margin-lef
 	['BEM', 'BEM 720'],
 	['Vinland', 'Vinland Saga 720'],
 	['SAO', 'Sword Art Online War of Underworld 720'],
-	['Food Wars', 'Shokugeki no Souma: Shin no Sara 720'],
+	['Food Wars', 'https://nyaa.si/?f=0&c=0_0&q=%5BJudas%5D+Food+Wars%21+S4', true],
 	['Fairy Gone', 'Fairy Gone 720']];
 
 
