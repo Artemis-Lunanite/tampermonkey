966667e5d8a3e8fe9468b18c2a2f376156cddb0a
Kyle Crossman <kcross199@gmail.com>
2019-12-17 20:09:25 -0500

    Update customize.js

--- xdcc horriblesubs/customize.js
+++ xdcc horriblesubs/customize.js
@@ -16,7 +16,6 @@ GM_addStyle('.header h1 {display: inline !important; } .kc_container{ margin-lef
 (function() {
     'use strict';
 	var searchOptions = [
-        ['Dr. Stone', 'Dr. Stone 720'],
 	['Fire Force', 'Enen no Shouboutai 720'],
 	['Vinland', 'Vinland Saga 720'],
 	['SAO', 'Sword Art Online War of Underworld 720'],
