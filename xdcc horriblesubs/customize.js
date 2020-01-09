80eeedfb337b6b32860162b89cd5676a50530235
Kyle Crossman <kcross199@gmail.com>
2020-01-09 11:18:49 -0500

    Update customize.js

--- xdcc horriblesubs/customize.js
+++ xdcc horriblesubs/customize.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HorribleSubs Customize
 // @namespace    http://tampermonkey.net/
-// @version      1.56124
+// @version      1.56125
 // @description  Changes the background on xdcc.horriblesubs.info.
 // @author       Artemis Lunanite
 // @match        *xdcc.horriblesubs.info/
@@ -16,16 +16,11 @@ GM_addStyle('.header h1 {display: inline !important; } .kc_container{ margin-lef
 (function() {
     'use strict';
 	var searchOptions = [
-	['Fire Force', 'Enen no Shouboutai 720'],
-	['Vinland', 'Vinland Saga 720'],
-	['SAO', 'Sword Art Online War of Underworld 720'],
-	['Food Wars', 'https://nyaa.si/?f=0&c=0_0&q=%5BJudas%5D+Food+Wars%21+S4', true],
-	['No Guns', 'https://nyaa.si/?f=0&c=0_0&q=Judas+no+guns+life', true],
-	['Fairy Gone', 'Fairy Gone 720'],
-	['Plunderer', ''],
-	['Darwins', ''],
-	['Dorohedoro', ''],
-	['Madoka Magica', '']];
+	['Plunderer', 'https://nyaa.si/?f=0&c=0_0&q=%5BJudas%5D+Plunderer'],
+	['Darwins', 'https://nyaa.si/?f=0&c=0_0&q=%5BJudas%5D+Darwin%27s+Game', true],
+	['Dorohedoro', 'https://nyaa.si/?f=0&c=0_0&q=%5BJudas%5D+Dorohedoro', true],
+	['Madoka Magica', 'https://nyaa.si/?f=0&c=0_0&q=%5BJudas%5D+Madoka', true],
+	['Nekopara', 'https://nyaa.si/?f=0&c=0_0&q=%5BJudas%5D+Nekopara', true];
 
 
     function addAnimeButtons(callback) {
